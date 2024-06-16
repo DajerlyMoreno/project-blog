aad6b7a28960fbf7359c496740a265160c6021fb
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-06-16 00:21:03 -0500

    Change left-right form flex order, and add funcionality to signup page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,46 +1,49 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
-import { HiMail } from "react-icons/hi";
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { HiMail } from 'react-icons/hi';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Debe llenar todos los campos');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-        {/* {left} */}
-        <div className="flex-1 order-2 md:order-1">
-          <form action="" className="flex flex-col gap-4">
-            <div>
-              <Label value="Tu nombre de usuario" />
-              <TextInput
-                type="text"
-                placeholder="Nombre de usuario"
-                id="username"
-              />
-            </div>
-            <div>
-              <Label value="Tu correo electrónico" />
-              <TextInput
-                type="email"
-                icon={HiMail}
-                placeholder="tucorreo@correo.com"
-                id="email"
-              />
-            </div>
-            <div>
-              <Label value="Tu contraseña" />
-              <TextInput type="password" placeholder="contraseña" id="password" />
-            </div>
-            <Button gradientDuoTone="greenToBlue" type="submit">Registrarse</Button>
-          </form>
-          <div className="flex gap-2 text-sm mt-2">
-            <span>
-              ¿Tienes una cuenta?
-            </span>
-            <Link to='/sign-in' className="underline text-blue-600">Iniciar sesión
-            </Link>
-          </div>
-        </div>
         {/* right */}
         <div className="flex-1 order-1 md:order-2">
           <Link to="/">
@@ -55,6 +58,64 @@ export default function SignUp() {
           <p className="text-sm mt-5 mx-auto text-center px-12">
             Conoce y opina sobre lo mejor de la gastronomía local
           </p>
+        </div>
+        {/* left */}
+        <div className="flex-1 order-2 md:order-1" onSubmit={handleSubmit}>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+          <form action="" className="flex flex-col gap-4">
+            <div>
+              <Label value="Tu nombre de usuario" />
+              <TextInput
+                type="text"
+                placeholder="Nombre de usuario"
+                id="username"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Tu correo electrónico" />
+              <TextInput
+                type="email"
+                icon={HiMail}
+                placeholder="tucorreo@correo.com"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Tu contraseña" />
+              <TextInput
+                type="password"
+                placeholder="contraseña"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone="greenToBlue"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Cargando</span>
+                </>
+              ) : (
+                'Registrarse'
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-2">
+            <span>¿Tienes una cuenta?</span>
+            <Link to="/sign-in" className="underline text-blue-600">
+              Iniciar sesión
+            </Link>
+          </div>
         </div>
       </div>
     </div>
