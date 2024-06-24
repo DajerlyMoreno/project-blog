b6c9179cbfea17bf568667bc11f4a0f0e0a20fb8
Jazmin-Moreno <jazmin.moreno01@uptc.edu.co>
2024-06-23 22:51:02 -0500

    post recientes

--- src/components/DescriptionAndAllPost.jsx
+++ src/components/DescriptionAndAllPost.jsx
@@ -3,20 +3,20 @@ import React from 'react';
 
 export default function DescriptionAndAllPost() {
   return (
-    <div className='w-full h-96 flex bg-gradient-to-r from-orange-300 to-purple-100 items-center'>
-      <div className='w-1/3 h-full'>
-        <img src="../../public/logo-neo.png" className='w-full h-full object-cover rounded-tr-full'/>
+    <div className='w-full h-auto flex flex-col sm:flex-row bg-gradient-to-r from-orange-300 to-purple-100 items-center'>
+      <div className='w-full sm:w-1/3 h-96'>
+        <img src="../../public/logo-neo.png" className='w-full h-full object-cover'/>
       </div>
-      <div className='w-2/3 h-full flex flex-col justify-center items-center p-7 text-lg sm:text-2xl text-center'>
-        <p className="mb-16">
+      <div className='w-full sm:w-2/3 h-auto flex flex-col justify-center items-center p-7 text-lg sm:text-2xl text-center'>
+        <p className="mb-4">
           ¡Bienvenido a NEO, tu destino culinario definitivo! Explora con nosotros los 
           rincones más deliciosos donde el café es sublime, las cenas son épicas y los 
           almuerzos son simplemente inigualables. En NEO, tu opinión cuenta tanto como 
           tu apetito. ¡Únete a nuestra comunidad y déjate llevar por un viaje 
           gastronómico sin límites!
         </p>
-        <Button className='rounded-full'>
-          <a href="">
+        <Button className='rounded-full px-8 py-4 text-lg sm:text-2xl'>
+          <a href="/search">
             VER TODOS LOS POST
           </a>
         </Button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DescriptionAndAllPost from '../components/DescriptionAndAllPost'
 
 export default function Home() {
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() =>{
+    try {
+      const fetchPosts = async () => {
+        const rest = await fetch(`/api/post/getPosts`)
+        const data = await rest.json()
+        setPosts(data.posts)
+
+      }
+      fetchPosts()
+    } catch (error) {
+      console.log(error)
+    }
+  },[])
+
   return (
     <div>
       <div className='w-full'>
         <DescriptionAndAllPost />
+      </div>
+        
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+        {posts && posts.length > 0 && (
+          <div className='flex flex-col justify-center items-center mb-5 '>
+            <h1 className='text-xl mt-10 font-semibold'>¡EXPLORA LA ULTIMO!</h1>
+            <div className='flex flex-wrap gap-4'>
+              {posts.map((post) =>{
+                <PostTarget key = {post.id} post = {post}/>
+              })}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
