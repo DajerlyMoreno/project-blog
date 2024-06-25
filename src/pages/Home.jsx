0ba41eaa7f78bbc1acbf2c73752c5653b39abdcb
Jazmin-Moreno <jazmin.moreno01@uptc.edu.co>
2024-06-24 20:36:29 -0500

    ?

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ export default function Home() {
       </div>
         
       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
-        {posts && posts.length == 0 && (//>////////////////////////////////////////////////////////
+        {posts && posts.length > 0 && (//>////////////////////////////////////////////////////////
           <div className='flex flex-col justify-center items-center mb-5 '>
             <h1 className='text-xl mt-10 font-semibold'>¡EXPLORA LA ULTIMO!</h1>
             <div className='flex flex-wrap gap-4'>
