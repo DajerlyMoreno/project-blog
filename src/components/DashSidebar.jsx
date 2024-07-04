92c9a262bc6ac4931beb31b0e0b66abcd1d82770
Jazmin-Moreno <jazmin.moreno01@uptc.edu.co>
2024-07-03 19:12:13 -0500

    agrego pagina de posts al perdil

--- src/components/DashSidebar.jsx
+++ src/components/DashSidebar.jsx
@@ -1,13 +1,16 @@
 import { Sidebar } from 'flowbite-react';
 import { useEffect, useState } from 'react';
-import { HiArrowSmRight, HiUser } from 'react-icons/hi';
+import { HiArrowSmRight, HiDocumentText, HiUser } from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { signoutSuccess } from '../redux/user/userSlice';
+import { current } from '@reduxjs/toolkit';
+import { useSelector } from 'react-redux';
 
 export default function DashSidebar() {
   const location = useLocation();
   const [tab, setTab] = useState('');
+  const {currentUser} = useSelector(state => state.user);
   const dispatch = useDispatch();
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -32,18 +35,29 @@ export default function DashSidebar() {
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className='flex flex-col gap-1'>
           <Sidebar.Item
             as={Link}
             to="/dashboard?tab=profile"
             active={tab === 'profile'}
             icon={HiUser}
-            label={'Usuario'}
+            label={currentUser.isAdmin? 'Admin' : 'User'}
             labelColor="dark"
             className="cursor-pointer"
           >
             Perfil
           </Sidebar.Item>
+          {currentUser.isAdmin && (
+            <Sidebar.Item
+            as={Link}
+            to='/dashboard?tab=posts'
+            active={tab === 'post'}
+            icon={HiDocumentText}
+            >
+            Post
+            </Sidebar.Item>
+          )}
+          
           <Sidebar.Item
             icon={HiArrowSmRight}
             onClick={handleSignout}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   useEffect(() =>{
     try {
       const fetchPosts = async () => {
-        const rest = await fetch(`/api/post/getPosts`)
+        //const rest = await fetch(`/api/post/getPosts`)
         const data = await rest.json()
         setPosts(data.posts)
 
@@ -26,16 +26,11 @@ export default function Home() {
         <DescriptionAndAllPost />
       </div>
         
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
-        {posts && posts.length == 0 && (
+      <div className='max-w-7xl mx-auto p-3 flex flex-col gap-8 py-7'>
+        {posts && posts.length > 0 && (
           <div className='flex flex-col justify-center items-center mb-5 '>
             <h1 className='text-xl mt-10 font-semibold'>¡EXPLORA LA ULTIMO!</h1>
             <div className='w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 pt-10'>
-              <PostTarget/>
-              <PostTarget/>
-              <PostTarget/>
-              <PostTarget/>
-              <PostTarget/>
               {posts.map((post) =>{
                 <PostTarget key = {post.id} post = {post}/>
               })}
