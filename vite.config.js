3eafc09ac986b9a2e5d288996ad5561f2b89f471
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-07-23 19:19:25 -0500

    update proxy target URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        // target: 'http://localhost:3000',
+        target: 'https://back-blog-project.onrender.com/',
         secure: false,
       },
     },
