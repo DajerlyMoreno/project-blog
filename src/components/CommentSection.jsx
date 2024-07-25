f96596e3b8e316902178839d54b8b92fda5f3189
SantiagoSalamancaa <bolthgg@gmail.com>
2024-07-25 00:56:07 -0500

    add edit button functionality

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -72,13 +72,35 @@ export default function CommentSection({ postId }) {
                 )
                 );
             }
-
-
         } catch (error) {
             console.log(error.message);
         }
-
-    }
+    };
+    const handleEdit = async (comment, editedContent) => {
+        setComments(
+            comments.map((c) =>
+                c._id === comment._id ? { ...c, content: editedContent } : c
+            )
+        );
+    };
+    const handleDelete = async (commentId) => {
+        setShowModal(false);
+        try {
+            if (!currentUser) {
+                navigate('/sign-in');
+                return;
+            }
+            const res = await fetch(`/api/comment/deleteComment/${commentId}`, {
+                method: 'DELETE',
+            });
+            if (res.ok) {
+                const data = await res.json();
+                setComments(comments.filter((comment) => comment._id !== commentId));
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
     return (
         <div className="max-w-2xl mx-auto w-full p-3">
             {currentUser ? (
@@ -128,7 +150,7 @@ export default function CommentSection({ postId }) {
                         </div>
                     </div>
                     {comments.map(comment => (
-                        <Comment key={comment._id} comment={comment} onLike={handleLike} />
+                        <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit}/>
                     ))}
                 </>
             )}
